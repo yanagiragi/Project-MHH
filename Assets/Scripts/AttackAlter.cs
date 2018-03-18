3a7c8c481163ed46e064ea0a47ff05bbb2a82ce0
yanagiragi <qaz90473@gmail.com>
2018-03-18 12:01:48 +0800

    refactor attackAlter & update simple trigger detect

--- Assets/Scripts/AttackAlter.cs
+++ Assets/Scripts/AttackAlter.cs
@@ -4,26 +4,18 @@ using UnityEngine;
 
 public class AttackAlter : MonoBehaviour {
 
-    // 第0個必須為0 最後一個很小值
-    [Tooltip("x 到 x + 1 的值代表要打出這個連段要在前一個動作的x秒後, x+1秒之前按")]
-    public Vector3[] intervalBetweenAttacks;
-    
-    [Header("(forward, backward, left, right)")]
-    public Vector4 scaleFactor = Vector4.one;
-
-    [Header("(forward, backward, left, right)")]
-    public Vector4 rollScaleFactor = Vector4.one;
-
-    public float runScale = 1.1f;
+    [SerializeField]
+    private bool isDebug;
 
     [Header("Control")]
+
     [SerializeField]
     private float inputH;
     [SerializeField]
     private float inputV;
 
-    //[SerializeField]
     public bool canWalk = true;
+
     [SerializeField]
     private bool isWalk = true;
     [SerializeField]
@@ -35,6 +27,11 @@ public class AttackAlter : MonoBehaviour {
     private bool isRoll = false;
     
     [Header("Attack")]
+
+    // 改掉Z數值要重調 晚點再弄
+    [Tooltip("x, y, z 沒有用")]
+    public Vector3[] intervalBetweenAttacks;
+
     [SerializeField]
     public bool canStartAttackFlag = true;
     //[SerializeField]
@@ -44,16 +41,16 @@ public class AttackAlter : MonoBehaviour {
     [SerializeField]
     private float innerTime;
 
+    [Header("Others")]
+
     [SerializeField]
     public bool isDrinking = false;
 
     [SerializeField]
     private bool isHit = false;
 
-    private float threshold = 0.05f;
-    private float epsilon = 0.01f;
     private Animator animController;
-
+    
     void Start () {
         innerTime = 0;
         animController = GetComponent<Animator>();
@@ -92,10 +89,10 @@ public class AttackAlter : MonoBehaviour {
 
         if (isHit)
         {
+            // Reset State
             canWalk = false;
             isWalk = false;
             isRun = false;
-            //animController.SetBool("Run", isRun);
             isDrinking = false;
             canRoll = false;
             isRoll = false;
@@ -108,33 +105,15 @@ public class AttackAlter : MonoBehaviour {
             animController.SetLayerWeight(1, 0);
             return;
         }
-
-        ////float mouseX = Input.GetAxis("Mouse X");
-        ////transform.Rotate(new Vector3(0, mouseX, 0));
-
-        //float mouseX2 = Input.GetAxis("Horizontal");
-        //transform.Rotate(new Vector3(0, mouseX2, 0));
-
-        //#region Walk Part
-
+        
+        // Hold input for deciding roll or not
         inputH = Input.GetAxis("Horizontal");
         inputV = Input.GetAxis("Vertical");
-
-        //isWalk = ((Mathf.Abs(inputH) - epsilon) > threshold) || ((Mathf.Abs(inputV) - epsilon) > threshold);
-        //isWalk &= animController.GetInteger("Attack") == 0 && animController.GetLayerWeight(1) == 1 && canWalk;
-
-        //animController.SetBool("Walk", isWalk);
-        ////animController.SetFloat("InputH", inputH);
-        //animController.SetFloat("InputV", inputV);
-
-        //isRun = Input.GetKey(KeyCode.LeftShift);
-
+        
         if (Input.GetKeyDown(KeyCode.Q) && canRoll && !isRoll && canStartAttackFlag && !startAttackFlag)
         {
             animController.SetBool("UsingItem", true);
             isDrinking = true;
-            //StartCoroutine(DelayAdjustWeight(2, 0));
-            //StartCoroutine(DelayAdjustWeight("UsingItem", false, -1, delegate { animController.SetLayerWeight(2, .46f); }, 1f));
             StartCoroutine(PreformDrink());
         }
 
@@ -151,45 +130,9 @@ public class AttackAlter : MonoBehaviour {
                 canWalk = false;
                 isWalk = false;
 
-                // -1f for end of frame
-                //StartCoroutine(DelayAdjustWeight("Roll", false, .3f, delegate { isRoll = false; }, .8f, delegate { canRoll = true; canWalk = true; }));
                 StartCoroutine(PerformRoll());
             }
         }
-
-        //animController.SetBool("Run", isRun);
-
-        ////inputH = Mathf.Lerp(inputH, Input.GetAxis("Horizontal"), Time.deltaTime);
-        ////inputV = Mathf.Lerp(inputV, Input.GetAxis("Vertical"), Time.deltaTime);
-
-
-        //// 收劍的動作要讓腳sync的上半身layer影響小一點
-        //if (isWalk && !isHit)
-        //{
-        //    float scaleH = (inputH > 0f) ? scaleFactor.w : scaleFactor.z;
-        //    float scaleV = (inputV > 0f) ? scaleFactor.x : scaleFactor.y;
-
-        //    if (isRun)
-        //    {
-        //        if (inputV < 0f)
-        //            scaleV *= 1.1f;
-        //        else
-        //            scaleV *= runScale;
-
-        //        Debug.Log("Run");
-
-        //        //animController.speed = 2;
-        //    }
-        //    //else
-        //    //{
-        //    //    animController.speed = 1;
-        //    //}
-
-        //    transform.position = Vector3.Lerp(transform.position, transform.position + transform.forward * scaleV * inputV, Time.deltaTime);
-        //    //transform.position = Vector3.Lerp(transform.position, transform.position + transform.right * scaleH * inputH, Time.deltaTime);
-        //}
-
-        //#endregion
 
         #region Attack Part
         if (startAttackFlag)
@@ -199,7 +142,8 @@ public class AttackAlter : MonoBehaviour {
 
         if (Input.GetMouseButtonDown(0) && canStartAttackFlag)
         {
-            Debug.Log("Detect Combo at innerTime: " + innerTime);
+            if(isDebug)
+                Debug.Log("Detect Combo at innerTime: " + innerTime);
             
             // 第一個連段
             if (!startAttackFlag && animController.GetBool("Draw")) // First Attack
@@ -214,17 +158,21 @@ public class AttackAlter : MonoBehaviour {
                 if (attackNum < intervalBetweenAttacks.Length && innerTime > intervalBetweenAttacks[attackNum].x && innerTime <= intervalBetweenAttacks[attackNum].y)
                 {
                     ++attackNum;
-                    Debug.Log("Perform Combo at innerTime: " + innerTime);
-                    Debug.Log("Perform Combo: " + attackNum);
+
                     animController.SetInteger("Attack", attackNum);
                     innerTime = 0;
 
-                    if(attackNum < intervalBetweenAttacks.Length)
+                    if (isDebug)
                     {
-                        Debug.Log("Press After   " + intervalBetweenAttacks[attackNum].x + " seconds.");
-                        Debug.Log("Press Befeore " + intervalBetweenAttacks[attackNum].y + " seconds.");
-                    }                    
+                        Debug.Log("Perform Combo at innerTime: " + innerTime);
+                        Debug.Log("Perform Combo: " + attackNum);
 
+                        if (attackNum < intervalBetweenAttacks.Length)
+                        {
+                            Debug.Log("Press After   " + intervalBetweenAttacks[attackNum].x + " seconds.");
+                            Debug.Log("Press Befeore " + intervalBetweenAttacks[attackNum].y + " seconds.");
+                        }
+                    }
                 }
             }
         }
@@ -233,10 +181,11 @@ public class AttackAlter : MonoBehaviour {
         if (
             !isRoll &&
             startAttackFlag && 
-            (innerTime > intervalBetweenAttacks[attackNum].y || attackNum > intervalBetweenAttacks.Length - 1)
-        )
-        {
-            Debug.Log("Combo " + attackNum + " Time Expires");
+            (attackNum > intervalBetweenAttacks.Length - 1 || innerTime > intervalBetweenAttacks[attackNum].y)
+        ){
+            if (isDebug)
+                Debug.Log("Combo " + attackNum + " Time Expires");
+
             StartCoroutine(restoreFlag(attackNum));
 
             attackNum = 0;
@@ -262,84 +211,12 @@ public class AttackAlter : MonoBehaviour {
         yield return new WaitForSeconds(delay);
         animController.SetLayerWeight(1, 1);
 
-        
+        if (isDebug)
+            Debug.Log("Roll Done");
 
-        //// StartCoroutine(DelayAdjustWeight("Roll", false, .3f, delegate { isRoll = false; }, .8f, delegate { canRoll = true; canWalk = true; }));
-
-        //float time = 0;
-
-        ////float h = 0.0f;
-        ////float v = 1.0f;
-
-        //float h = inputH;
-        //float v = inputV;
-
-        ////transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(h, 0f, v), Vector3.up), Time.deltaTime);
-        ////Vector3 trans = transform.TransformDirection(new Vector3(inputH, 0f, inputV));
-        ////Vector3 newVec = new Vector3(inputH, 0f, inputV);
-        ////transform.rotation = Quaternion.LookRotation(trans, Vector3.up);
-        
-
-        //while (time < 0.3f)
-        //{
-        //    time += Time.deltaTime;
-        //    float scaleH = (h > 0f) ? rollScaleFactor.w : rollScaleFactor.z;
-        //    float scaleV = (v > 0f) ? rollScaleFactor.x : rollScaleFactor.y;
-        //    //scaleV = scaleV * (v > 0 ? 1f : -1f);
-        //    //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(h, 0f, v), Vector3.up), Time.deltaTime);
-        //    transform.position = Vector3.Lerp(transform.position, transform.position + transform.forward * scaleV, Time.deltaTime);
-        //    //transform.position = Vector3.Lerp(transform.position, transform.position + transform.right * scaleH, Time.deltaTime);
-        //    yield return null;
-        //}
-
-        ////yield return new WaitForSeconds(.3f);
-
-        //animController.SetBool("Roll", false);
-        //isRoll = false;
-
-        //time = 0;
-        //while (time < 0.9f)
-        //{
-        //    time += Time.deltaTime;
-        //    float scaleH = (h > 0f) ? rollScaleFactor.w : rollScaleFactor.z;
-        //    float scaleV = (v > 0f) ? rollScaleFactor.x : rollScaleFactor.y;
-        //    //scaleV = scaleV * (v > 0 ? 1f : -1f);
-        //    transform.position = Vector3.Lerp(transform.position, transform.position + transform.forward * scaleV , Time.deltaTime);
-        //    //transform.position = Vector3.Lerp(transform.position, transform.position + transform.right * scaleH, Time.deltaTime);
-        //    yield return null;
-        //}
-
-        Debug.Log("Done");
-
-        //yield return new WaitForSeconds(.8f);
         canRoll = true;
         canWalk = true;
         isRoll = false;
-
-    }
-
-    IEnumerator DelayAdjustWeight(string stateName, bool result, float delay, callback callbackFunc = null, float secondaryDelay = 0f, callback SecondcallbackFunc=null)
-    {
-        if(delay < 0f)
-            yield return new WaitForEndOfFrame();
-        else
-            yield return new WaitForSeconds(delay);
-
-        animController.SetBool(stateName, result);
-
-        if (callbackFunc != null)
-        {
-            callbackFunc();
-        }
-
-        if (SecondcallbackFunc != null)
-        {
-            if (secondaryDelay < 0f)
-                yield return new WaitForEndOfFrame();
-            else
-                yield return new WaitForSeconds(secondaryDelay);
-            SecondcallbackFunc();
-        }
 
     }
 
@@ -348,12 +225,6 @@ public class AttackAlter : MonoBehaviour {
         yield return new WaitForEndOfFrame();
 
         animController.SetBool("UsingItem", false);
-
-        //float delay = 1.1f;
-
-        //yield return new WaitForSeconds(delay);
-
-        //isDrinking = true;
     }
 
     IEnumerator DelayAdjustWeight(int layer, float weight)
@@ -361,23 +232,13 @@ public class AttackAlter : MonoBehaviour {
         float delay = 0.1f;
         yield return new WaitForSeconds(delay);
         animController.SetLayerWeight(layer, weight);
-
-        // Need More tweak on walk->Attake Fade
-
-        // Below: weight drop too fast
-        //float initWeight = animController.GetLayerWeight(layer);
-        //float time = 0;
-        //while((time += Time.deltaTime) < delay && animController.GetLayerWeight(layer) > weight)
-        //{
-        //    animController.SetLayerWeight(layer, (initWeight - weight) / delay * Time.deltaTime);
-        //    yield return null;
-        //}
-
     }
 
     IEnumerator restoreFlag(int atk)
     {
-        Debug.Log("End Combo: " + atk);
+        if(isDebug)
+            Debug.Log("End Combo: " + atk);
+
         if (atk == 4)
         {
             attackNum = 0;
@@ -391,57 +252,40 @@ public class AttackAlter : MonoBehaviour {
         }
         else if (atk == 3)
         {
-            // original value: 1.2f, 1.5f, 2f
             float firstTime = .05f;
             float secondTime = 1.0f;
-            //float totalTime = 1.35f;
-
+            
             yield return new WaitForSeconds(firstTime);
             attackNum = 0;
-            animController.SetInteger("Attack", attackNum); // Addition Adjust Attack to Sync Leg Layer
+            animController.SetInteger("Attack", attackNum);
 
             yield return new WaitForSeconds(secondTime - firstTime);
             canStartAttackFlag = true;
 
-            //yield return new WaitForSeconds(totalTime - secondTime);
             if (animController.GetLayerWeight(1) != 1f && attackNum == 0)
                 animController.SetLayerWeight(1, 1);
         }
         else if (atk == 2)
         {
-            // original value: .2f, .4f
             float firstTime = .2f;
             float totalTime = .3f;
 
             yield return new WaitForSeconds(firstTime);
-
             canStartAttackFlag = true;
-            //yield return new WaitForSeconds(totalTime - firstTime);
+
             if (animController.GetLayerWeight(1) != 1f && attackNum == 0)
                 animController.SetLayerWeight(1, 1);
         }
         else if (atk == 1)
         {
-            // original value: .2f, .6f
             float firstTime = .1f;
             float totalTime = .5f;
 
             yield return new WaitForSeconds(totalTime);
             canStartAttackFlag = true;
 
-            //yield return new WaitForSeconds(totalTime - firstTime);            
             if (animController.GetLayerWeight(1) != 1f && attackNum == 0)
                 animController.SetLayerWeight(1, 1);
         }
-
-        //GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter>().canWalk = true;
-    }
-
-    // Code Need Refactor
-    IEnumerator delayRoutine()
-    {
-        animController.SetBool("Roll", true);
-        yield return new WaitForEndOfFrame();
-        animController.SetBool("Roll", false);
     }
 }
